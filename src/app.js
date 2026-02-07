ab065fb3db2a64f5b7ef01826dee4f3198a5085d
Veerman14 <hasibuanfirman1404@gmail.com>
2026-02-07 16:26:21 +0700

    chore: register URL routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
 import { config } from "./config/config.js";
 
 import authRoutes from "./routes/auth.routes.js";
+import urlRoutes from "./routes/url.routes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
@@ -12,6 +15,7 @@ app.get("/health-check", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/url", urlRoutes);
 
 app.use(errorHandler);
 
