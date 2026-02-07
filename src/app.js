ac68b8c76759e600fda5aac7a384271f5e71f45a
Veerman14 <hasibuanfirman1404@gmail.com>
2026-02-07 14:58:52 +0700

    chore: register auth routes and error handler

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 import express from "express";
 import { config } from "./config/config.js";
 
+import authRoutes from "./routes/auth.routes.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+
 const app = express();
 app.use(express.json());
 
 app.get("/health-check", (req, res) => {
     res.send("URL Shortener Backend is running");
 });
+
+app.use("/api/auth", authRoutes);
+
+app.use(errorHandler);
 
 app.listen(config.port, () => {
     console.log(`Server is running on port ${config.port}`);
