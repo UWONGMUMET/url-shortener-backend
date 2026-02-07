85272d4aba31d9f9e4f8326b725bc8951c94728a
Veerman14 <hasibuanfirman1404@gmail.com>
2026-02-07 16:20:19 +0700

    feat: update Prisma schema

---
+++ prisma/migrations/20260207084101/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the `URL` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "URL" DROP CONSTRAINT "URL_userId_fkey";
+
+-- DropTable
+DROP TABLE "URL";
+
+-- CreateTable
+CREATE TABLE "Url" (
+    "id" SERIAL NOT NULL,
+    "longUrl" TEXT NOT NULL,
+    "shortCode" TEXT NOT NULL,
+    "clicks" INTEGER NOT NULL DEFAULT 0,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Url_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Url_shortCode_key" ON "Url"("shortCode");
+
+-- AddForeignKey
+ALTER TABLE "Url" ADD CONSTRAINT "Url_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
