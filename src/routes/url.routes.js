8f1f52d0fb0b038a9b5c481e4f3e1ff0f552c7d1
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-10 16:09:25 +0700

    add url routes with analytics endpoints

--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { shortenUrl, redirectToOriginalUrl } from '../controllers/url.controller.js';
+import { shortenUrl, redirectToOriginalUrl, getUserUrls, getUrlAnalytics } from '../controllers/url.controller.js';
 import { authMiddleware } from '../middlewares/auth.js';
 
 const router = express.Router();
 
 router.post('/', authMiddleware, shortenUrl);
+router.get("/my", authMiddleware, getUserUrls);
+router.get("/:shortCode/analytics", authMiddleware, getUrlAnalytics);
 router.get('/:shortCode', redirectToOriginalUrl);
 
 export default router;
