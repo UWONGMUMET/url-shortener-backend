6f14eed2366b3bed6535218b275cd1c3b8781ff4
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-10 11:29:09 +0700

    feat: apply global api rate limiter

--- src/app.js
+++ src/app.js
@@ -6,16 +6,21 @@ import { config } from "./config/config.js";
 import authRoutes from "./routes/auth.routes.js";
 import urlRoutes from "./routes/url.routes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import { apiLimiter } from "./middlewares/rateLimiter.js";
+import { morganMiddleware } from "./middlewares/logger.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morganMiddleware);
 
 app.get("/health-check", (req, res) => {
     res.send("URL Shortener Backend is running");
 });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/url", urlRoutes);
+app.use("/api/url", apiLimiter, urlRoutes);
 
 app.use(errorHandler);
 
