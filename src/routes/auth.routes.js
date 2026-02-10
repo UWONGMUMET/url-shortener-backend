2d2a4f0fecb311521ad2230c0160ad750ec82652
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-10 13:21:12 +0700

    feat: add logout routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { register, login } from "../controllers/auth.controller.js";
+import { register, login, logout } from "../controllers/auth.controller.js";
 import { authLimiter } from "../middlewares/rateLimiter.js";
+import { authMiddleware } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/register", authLimiter, register);
 router.post("/login", authLimiter, login);
+router.post("/logout", authMiddleware, logout);
 
 export default router;
